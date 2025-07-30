00ed206328aea50a2b2fef2bc9cde55e555bab3a
jasminesamra <jasminekaursamra@gmail.com>
2025-07-30 13:04:05 -0700

    add samantha sketch

--- samantha/sketch.js
+++ samantha/sketch.js
@@ -1,27 +1,47 @@
-let circleSize, time, stepSize;
+let x, y, size, timeX, timeY, img;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  circleSize = width / 4;
-  time = random(0, 1000);
-  stepSize = 10;
+  x = width / 2;
+  y = width / 2;
+  size = 40; //star size of circle
+
+  timeX = 0;
+  timeY = 10000;
+  //img = loadImage('smitskisitting.jpg')
 }
 
 function draw() {
-  background('#aaaaaa');
-
-  fill('#fdbde0ff');
-
-  stroke('#affe90ff');
-
-  strokeWeight(5);
-
-  let x = 300;
-  let y = height / 2;
-
-  let unmappedSize = noise(time);
-  let size = map(unmappedSize, 0, 1, 0, width / 4);
-  time += 0.001 * stepSize;
-
-  circle(x, y, size);
+  background(200);
+  size += 1; //increasing size of circle
+  moveAndDrawCircle();
+  //drawThoughtBubble();
+  //image(img, 0, 0);
 }
+
+let moveAndDrawCircle = () => {
+  stepSize = 10;
+
+  updateCoordinatesWithRandom();
+
+  fill(255, 0, 0);
+  circle(x, y, size);
+};
+
+let updateCoordinatesWithRandom = () => {
+  x += random(-stepSize, stepSize);
+  x = constrain(x, 0, width);
+
+  y += random(-stepSize, stepSize);
+  y = constrain(y, 0, width);
+};
+
+let updateCoordinatesWithPerlin = () => {
+  let unmappedX = noise(timeX);
+  x = map(unmappedX, 0, 1, 0, width);
+  timeX += 0.001 * stepSize;
+
+  let unmappedY = noise(timeY);
+  y = map(unmappedY, 0, 1, 0, width);
+  timeY += 0.001 * stepSize;
+};
