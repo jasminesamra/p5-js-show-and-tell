7e48719c905b5f66d9e87f87d4884940a08e90e4
jasminesamra <jasminekaursamra@gmail.com>
2025-07-30 12:25:37 -0700

    add slider for step size for perlin dancers

--- jasmine/perlinDancers.js
+++ jasmine/perlinDancers.js
@@ -1,43 +1,57 @@
 let circles = [];
-let slider;
-let numCircles;
-let prevNumCircles;
+let numCircles, prevNumCircles, numCirclesSlider;
+let stepSize, prevStepSize, stepSizeSlider;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  slider = createSlider(1, 100, 2, 1);
-  slider.position(10, 10);
-  slider.size(width / 2);
+  numCirclesSlider = createSlider(1, 100, 2, 1);
+  numCirclesSlider.position(10, 10);
+  numCirclesSlider.size(width / 2);
 
-  numCircles = slider.value();
+  numCircles = numCirclesSlider.value();
+  prevNumCircles = numCircles;
+
+  stepSizeSlider = createSlider(1, 20, 2, 1);
+  stepSizeSlider.position(10, 60);
+  stepSizeSlider.size(width / 4);
+
+  stepSize = stepSizeSlider.value();
+  prevStepSize = stepSize;
 
   populateCircles();
+  printValues();
 }
 
 function draw() {
-  numCircles = slider.value();
+  numCircles = numCirclesSlider.value();
   if (numCircles != prevNumCircles) {
-    background("#ffffff");
     prevNumCircles = numCircles;
+    background("#ffffff");
     circles = [];
     populateCircles();
+    printValues();
   }
+
+  stepSize = stepSizeSlider.value();
+    if (stepSize != prevStepSize) {
+      prevStepSize = stepSize;
+      background('#ffffff');
+      printValues();
+    }
 
   // for each circle in array circles, update coordinates and draw
   circles.map(moveAndDrawCircle);
-
-  text(`dancers: ${numCircles}`, 20, 40);
 }
 
 let moveAndDrawCircle = (c) => {
   let nextXValue = noise(c.timeX);
   c.x = map(nextXValue, 0, 1, 0, width);
-  c.timeX += 0.005;
+  c.timeX += 0.001 * stepSize;
 
   let nextYValue = noise(c.timeY);
   c.y = map(nextYValue, 0, 1, 0, height);
-  c.timeY += 0.005;
+  c.timeY += 0.001 * stepSize;
 
   circle(c.x, c.y, 40);
 };
@@ -52,3 +66,8 @@ const populateCircles = () => {
     });
   }
 };
+
+const printValues = () => {
+text(`dancers: ${numCircles}`, 20, 35);
+text(`step size: ${stepSize}`, 20, 85);
+}
