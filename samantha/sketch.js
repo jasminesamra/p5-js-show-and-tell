af74dfda35dbea10156aa3bc5ee0f4349d0a97be
jasminesamra <jasminekaursamra@gmail.com>
2025-07-31 09:21:37 -0700

    update samantha; add lauren; update canvas setting so no scrollbars show on full-window canvas

--- samantha/sketch.js
+++ samantha/sketch.js
@@ -1,22 +1,55 @@
-let x, y, size, timeX, timeY, img;
+let x, y, size, timeX, timeY, img, img2;
+let delayTime = 1000; //2 seconds delay
+let startTime;
+let showImageTwo = false;
+
+function preload() {
+  //loading in image
+  img = loadImage('IMG_7050.PNG');
+  img2 = loadImage('IMG_3815.PNG');
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.style('display', 'block');
+  
   x = width / 2;
   y = width / 2;
   size = 40; //star size of circle
 
   timeX = 0;
   timeY = 10000;
-  //img = loadImage('smitskisitting.jpg')
+
+  //making a timer
+  startTime = millis(); //record when program starts
 }
 
 function draw() {
   background(200);
-  size += 1; //increasing size of circle
-  moveAndDrawCircle();
-  //drawThoughtBubble();
-  //image(img, 0, 0);
+
+  //showing first image
+  if (!showImageTwo) {
+    //displaying where the image goes
+    imageMode(CENTER);
+    image(img, width / 2, height / 2, 250, 286);
+  }
+
+  //wait for delaytime to pass before showing panic (red circle)
+  if (millis() - startTime > delayTime) {
+    size += 5; //increase size of circle
+    moveAndDrawCircle();
+
+    //check if red circle filled the screen
+    if (size >= max(width, height) * 1.75) {
+      showImageTwo = true;
+    }
+  }
+
+  //if red filled screen, show second image
+  if (showImageTwo) {
+    imageMode(CENTER);
+    image(img2, width / 2, height / 2, 251, 100);
+  }
 }
 
 let moveAndDrawCircle = () => {
